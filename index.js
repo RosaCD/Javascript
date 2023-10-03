5523a0fa07d0794d217b5851b796d3ce13deb10d
RosaCD <rcruz12@ucol.mx>
2023-10-03 16:45:58 -0600

    First version

---
+++ index.js
@@ -0,0 +1,21 @@
+// Declaracion de variables en Javascript moderno
+// let number = 5;
+// number = 8;
+// const numero = 4;
+
+// Alerta en Javascript
+// alert("Hola desde una alerte de Javascript");
+// console.log("Esto es javascript desde html");
+// console.log(number);
+// console.log(numero);
+
+// Concatenacion
+let name = "Rosa";
+let lastName = " Decena";
+console.log("Mi nombre es:" + " " + name + lastName);
+
+// document.write("Mi nombre es:" + " " + name + lastName);
+
+let seleccion = document.querySelector("#seccion1");
+
+seleccion.innerHTML = ('<h3>Mi nombre es Rosa Elvira</h3>');
